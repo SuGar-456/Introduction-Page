c748f166ac206f3f777b85f323e6f4be2f7aacc7
SuGar-456 <shayingsdt@gmail.com>
2025-09-28 15:53:37 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === 'production'
-const repo = 'Introduction-Page'   
-
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',                 // 生成 out/ 静态站
-  basePath: isProd ? `/${repo}` : '',
-  assetPrefix: isProd ? `/${repo}/` : '',
-
- //trailingSlash: true,
+  output: 'export',   
 }
 module.exports = nextConfig
