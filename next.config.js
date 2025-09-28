5c35137178d1474769ed07a8048839a7b251d0e4
SuGar-456 <shayingsdt@gmail.com>
2025-09-28 15:37:56 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,10 +4,10 @@ const repo = 'Introduction-Page'
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',                
+  output: 'export',                 // 生成 out/ 静态站
   basePath: isProd ? `/${repo}` : '',
   assetPrefix: isProd ? `/${repo}/` : '',
-
-  trailingSlash: true,
+  // 可选：多页面时开启可减少 404
+  // trailingSlash: true,
 }
 module.exports = nextConfig
